ce6c96fe01199f95b8c5d6588f320a77cfa0fc3c
qijian1988 <qijian1988@yeah.net>
2017-06-29 23:38:46 +0800

    6.29

--- sell/src/main.js
+++ sell/src/main.js
@@ -1,15 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue';
-import App from './App';
-import VueRouter from 'vue-router';
-import goods from './components/goods/goods';
-import seller from './components/seller/seller';
-import ratings from './components/ratings/ratings';
-import './common/stylus/index.styl';
-Vue.use(VueRouter);
+import Vue from 'vue'
+import App from './App'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+import goods from './components/goods/goods'
+import seller from './components/seller/seller'
+import ratings from './components/ratings/ratings'
+Vue.use(VueRouter)
+Vue.use(VueResource)
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 
@@ -18,16 +19,16 @@ const routes = [
  { path: '/goods', component: goods },
  { path: '/ratings', component: ratings },
  { path: '/seller', component: seller }
- ];
+ ]
 
- const router = new VueRouter({
- routes,
- linkActiveClass: 'active'  // 设置被激活后的class名
- });
+const router = new VueRouter({
+routes,
+linkActiveClass: 'active'  // 设置被激活后的class名
+ })
 
 new Vue({
   el: '#app',
   template: '<App/>',
   components: { App },
   router
-}).$mount('#app');
+}).$mount('#app')
